5f3a784b122fd261c4ed70ea6a1474bcb4a47acf
thienth32 <thienth32@gmail.com>
2020-02-23 17:45:23 +0700

    update sidebar

--- admin/_share/sidebar.php
+++ admin/_share/sidebar.php
@@ -1,9 +1,9 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+        <img src="<?= ADMIN_ASSET_URL ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
              style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">ICID Complex</span>
     </a>
 
     <!-- Sidebar -->
@@ -11,10 +11,10 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="<?= BASE_URL . $_SESSION[AUTH]['avatar'] ?>" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <a href="#" class="d-block"><?= $_SESSION[AUTH]['name']?></a>
             </div>
         </div>
 
@@ -70,12 +70,43 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="<?= ADMIN_URL . 'users/add-form.php'?>" class="nav-link">
+                            <a href="<?= ADMIN_URL . 'vehicle_types/add-form.php'?>" class="nav-link">
                                 <i class="fa fa-plus nav-icon"></i>
                                 <p>Thêm loại phương tiện</p>
                             </a>
                         </li>
                     </ul>
+                </li>
+                <li class="nav-item has-treeview">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fa fa-motorcycle"></i>
+                        <p>
+                            Phương tiện
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="<?= ADMIN_URL . 'vehicles'?>" class="nav-link">
+                                <i class="fa fa-list-ol nav-icon"></i>
+                                <p>Danh sách</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="<?= ADMIN_URL . 'vehicles/add-form.php'?>" class="nav-link">
+                                <i class="fa fa-plus nav-icon"></i>
+                                <p>Thêm phương tiện</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item has-treeview">
+                    <a href="<?= BASE_URL . 'logout.php'?>" class="nav-link">
+                        <i class="nav-icon fas fa-sign-out-alt"></i>
+                        <p>
+                            Logout
+                        </p>
+                    </a>
                 </li>
             </ul>
         </nav>
